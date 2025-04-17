8debcb516882c9b66a7d781410c7017b3fedb8bd
Alyssa Anderson <wackyvorlon@gmail.com>
2025-04-17 18:46:51 -0400

    Added code to prevent output of real words.

--- wordgen.py
+++ wordgen.py
@@ -2,15 +2,26 @@
 
 import random
 
-
-lines = open('common-syllables.txt').read().splitlines()
-
-myline = random.choice(lines)
-
-numsyl = random.choice(range(3))
-
-for i in range(numsyl):
-    myline+=random.choice(lines)
+with open("/usr/share/dict/words") as f:
+    words = f.read().splitlines()
     
 
+def makeword():
+    
+    lines = open('common-syllables.txt').read().splitlines()
+
+    myline = random.choice(lines)
+
+    numsyl = random.choice(range(3))
+
+    for i in range(numsyl):
+        myline+=random.choice(lines)
+
+    return myline
+    
+myline = makeword()
+
+if myline in words:
+    myline = makeword()
+    
 print(myline)
