56feedf7de9331d4d938ad5763e15539fc3dbef7
Alyssa Anderson <wackyvorlon@gmail.com>
2025-04-17 18:29:30 -0400

    Tweaked syllable number. Avg syllable length in english is 1.6, I had the max at five. Now reduced to three.

--- wordgen.py
+++ wordgen.py
@@ -7,7 +7,7 @@ lines = open('common-syllables.txt').read().splitlines()
 
 myline = random.choice(lines)
 
-numsyl = random.choice(range(5))
+numsyl = random.choice(range(3))
 
 for i in range(numsyl):
     myline+=random.choice(lines)
